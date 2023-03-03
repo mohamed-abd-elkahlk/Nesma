91a3774ca7825076e32af77cd3cbb9dc124b135f
mohamed-abd-elkahlk <01147018091.m@gmail.com>
2023-03-03 23:39:17 +0200

    commit message

---
+++ js/main.js
@@ -0,0 +1,85 @@
+//!All div !//
+let welcomeText = document.querySelector(".welcome");
+let homePage = document.querySelector(".home");
+let header = document.getElementById("header");
+let aboutDiv = document.getElementById("about-div");
+let contact = document.querySelector(".contact");
+
+let since = document.querySelector(".since");
+
+let math = document.querySelector(".math");
+
+let english = document.querySelector(".english");
+
+//!All div !//
+//?//////////////////////////////////////////////////////////////////////////////////
+//!All Button !//
+let logIn = document.getElementById("logIn");
+let homeButton = document.getElementById("HomeButton");
+let aboutButton = document.getElementById("about");
+let sinceB = document.getElementById("sinceB");
+let HomeB = document.querySelector(".Home");
+let contactB = document.getElementById("contactB");
+
+let mathB = document.getElementById("math");
+let phyB = document.getElementById("phy");
+let englishB = document.getElementById("english");
+//!All Button !//
+
+HomeB.onclick = function () {
+  welcomeText.classList.add("d-none");
+  homePage.classList.remove("d-none");
+};
+
+homeButton.onclick = function () {
+  header.classList.remove("d-none");
+  aboutDiv.classList.add("d-none");
+  contact.classList.add("d-none");
+  since.classList.add("d-none");
+  english.classList.add("d-none");
+  math.classList.add("d-none");
+};
+
+aboutButton.onclick = function () {
+  header.classList.add("d-none");
+  aboutDiv.classList.remove("d-none");
+  contact.classList.add("d-none");
+  since.classList.add("d-none");
+  english.classList.add("d-none");
+  math.classList.add("d-none");
+};
+
+contactB.onclick = function () {
+  header.classList.add("d-none");
+  aboutDiv.classList.add("d-none");
+  contact.classList.remove("d-none");
+  since.classList.add("d-none");
+  english.classList.add("d-none");
+  math.classList.add("d-none");
+};
+mathB.onclick = function () {
+  header.classList.add("d-none");
+  aboutDiv.classList.add("d-none");
+  contact.classList.add("d-none");
+  since.classList.add("d-none");
+  english.classList.add("d-none");
+  math.classList.remove("d-none");
+};
+
+sinceB.onclick = function () {
+  header.classList.add("d-none");
+  aboutDiv.classList.add("d-none");
+  contact.classList.add("d-none");
+  since.classList.remove("d-none");
+  english.classList.add("d-none");
+  math.classList.add("d-none");
+};
+
+englishB.onclick = function () {
+  header.classList.add("d-none");
+  aboutDiv.classList.add("d-none");
+  contact.classList.add("d-none");
+  since.classList.add("d-none");
+  english.classList.remove("d-none");
+  math.classList.add("d-none");
+};
